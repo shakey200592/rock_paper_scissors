340724e6ec1124a9489bbc173efbd11a0c4f6e55
shakey200592 <shakey1992jm@hotmail.co.uk>
2024-05-27 20:03:01 +0100

    refactor: modify getComputerChoice function() and move choices array outside block

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-function getComputerChoice() {
-  // This function returns a random string from the values stored in the "choices" array
+const choices = ["rock", "paper", "scissors"]; // Store choices in a string array
 
-  const choices = ["rock", "paper", "scissors"]; // Store choices in a string array
-  let randomeChoice = Math.floor(Math.random() * choices.length); // Select random number between 0 and 2
-  return choices[randomeChoice]; // return one of the three values stores in the array based on the index
+function getComputerChoice(arr_of_choices) {
+  // This function returns a random string from the values stored in the "choices" array
+  let randomeChoice = Math.floor(Math.random() * arr_of_choices.length); // Select random number between 0 and 2
+  return `Computer Choice (${randomeChoice}): ${choices[randomeChoice].charAt(0).toUpperCase()+choices[randomeChoice].slice(1)}`; // return one of the three values stores in the array based on the index
 }
 
-function getHumanChoice() {
+function getHumanChoice(arr_of_choices) {
 /* 
   This function returns a string based on the number that is input to the string.
   There is Error checking to ensure that only a number can be entered and keeps
