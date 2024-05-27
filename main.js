1803de7a5a278e698233dc1208a805cafc88f617
shakey200592 <shakey1992jm@hotmail.co.uk>
2024-05-27 21:04:56 +0100

    feat: implement getWinner() function to calculate the winner of each game

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+// Variables
+
+const choices = ["rock", "paper", "scissors"]; // Store choices in a string array
+
+let computer_score;
+let player_score;
+
+console.log(getWinner(getComputerChoice(choices), getHumanChoice(choices)));
+
+
 function getComputerChoice(arr_of_choices) {
   // This function returns a random string from the values stored in the "choices" array
 
@@ -6,7 +16,7 @@ function getComputerChoice(arr_of_choices) {
 
   // Returns string based on the "arr_of_choices" parameter
   // and returns a string with the first char capitalised
-  return `Computer Choice (${randomeChoice}): ${capitalise_first_letter(choices[randomeChoice])}`;
+  return randomeChoice;
 }
 
 function getHumanChoice(arr_of_choices) {
@@ -22,16 +32,57 @@ function getHumanChoice(arr_of_choices) {
   {
     playerChoice = parseInt(prompt("Please enter a value between 0 and 2 (0 = Rock, 1 = Paper, 2 = Scissors)"));
   } while (isNaN(playerChoice) || playerChoice > 2 || playerChoice < 0);
-  return `Player Choice (${playerChoice}): ${capitalise_first_letter(choices[playerChoice])}`;
+  return playerChoice;
 }
 
-function capitalise_first_letter(string_to_capitalise)
+function getWinner(computer_choice, player_choice)
 {
-  return (string_to_capitalise.charAt(0).toUpperCase()+string_to_capitalise.slice(1));
+  // draw, return 0
+  if (computer_choice == player_choice)
+  {
+    console.log(`Computer choice (${computer_choice}): ${choices[computer_choice]}\nPlayer Choice (${player_choice}): ${choices[player_choice]}\nDraw`);
+    return 0;
+  }
+
+  // computer=rock and player=paper (player wins, return 2)
+  else if (computer_choice == 0 && player_choice == 1 )
+    {
+      console.log(`Computer choice (${computer_choice}): ${choices[computer_choice]}\nPlayer Choice (${player_choice}): ${choices[player_choice]}\n Player Wins`);
+      return 2;
+    }
+
+  // computer=rock and player=scissors (computer wins, return 1)
+  else if (computer_choice == 0 && player_choice == 2 )
+    {
+      console.log(`Computer choice (${computer_choice}): ${choices[computer_choice]}\nPlayer Choice (${player_choice}): ${choices[player_choice]}\n Computer Wins`);
+      return 1;
+    }
+
+  // computer=paper and player=rock (player wins, return 2)
+  else if (computer_choice == 1 && player_choice == 2 )
+    {
+      console.log(`Computer choice (${computer_choice}): ${choices[computer_choice]}\nPlayer Choice (${player_choice}): ${choices[player_choice]}\n Player Wins`);
+      return 2;
+    }
+
+  // computer=rock and player=scissors (player wins, return 2)
+  else if (computer_choice == 0 && player_choice == 2 )
+    {
+      console.log(`Computer choice (${computer_choice}): ${choices[computer_choice]}\nPlayer Choice (${player_choice}): ${choices[player_choice]}\n Player Wins`);
+      return 2;
+    }
+
+    // computer=scissors and player=rock (player wins, return 2)
+  else if (computer_choice == 2 && player_choice == 0 )
+    {
+      console.log(`Computer choice (${computer_choice}): ${choices[computer_choice]}\nPlayer Choice (${player_choice}): ${choices[player_choice]}\n Player Wins`);
+      return 2;
+    }
+
+  // computer=scissors and player=paper (computer wins, return 2)
+  else if (computer_choice == 2 && player_choice == 2 )
+    {
+      console.log(`Computer choice (${computer_choice}): ${choices[computer_choice]}\nPlayer Choice (${player_choice}): ${choices[player_choice]}\n Computer Wins`);
+      return 1;
+    }
 }
-
-const choices = ["rock", "paper", "scissors"]; // Store choices in a string array
-console.log(getComputerChoice(choices));
-console.log(getHumanChoice(choices));
-
-
