b27f4fedc60163251644390fb7527d7d97919be6
shakey200592 <shakey1992jm@hotmail.co.uk>
2024-05-31 17:23:30 +0100

    refactor: remove obselete functions since ui was built

--- main.js
+++ main.js
@@ -3,11 +3,6 @@
 const choices = ["rock", "paper", "scissors"]; // Store choices in a string array
 let computerScore = 0;
 let playerScore = 0;
-let numberOfRounds = 5;
-
-
-// Main game code
-playGame(numberOfRounds);
 
 
 // Functions for game functionality
@@ -22,51 +17,10 @@ function getComputerChoice(arr_of_choices) {
   return randomeChoice;
 }
 
-function getPlayerChoice() {
-/* 
-  This function returns a number based on the string entered
-  rock = 0, paper = 1, scissors = 2. input is case insensitive
-  thanks to the .toLowerCase() in-built function.
-*/
-  let playerChoice;
+function playRound(computerChoice, playerChoice) {
 
-  // Keeps asking for an input until it matches one of the values in choices array
-  do 
-  {
-    playerChoice = prompt("Please type either 'rock', 'paper' 'scissors' (case insensitive)").toLowerCase();
-  } while (choices.includes(playerChoice) == false)
+  return (computerChoice == 0 && playerChoice == 1 || computerChoice == 1 && playerChoice == 2 || computerChoice == 2 && playerChoice == 0) ? 2 : 1;
 
-  playerChoice = choices.indexOf(playerChoice);
-
-  return playerChoice;
-}
-
-function playRound(computerChoice, playerChoice)
-{
-  // This function includes if statements for all possible outcomes and
-  // outputs 0 for a draw 1 for computer win and 2 for a player win
-  console.log(computerChoice);
-  console.log(playerChoice);
-  // draw, return 0
-  if (computerChoice == playerChoice)
-  {
-    console.log(`Computer choice (${computerChoice}): ${choices[computerChoice]}\nPlayer Choice (${playerChoice}): ${choices[playerChoice]}\nDraw`);
-    return 0;
-  }
-
-  // player wins, return 2
-  else if (computerChoice == 0 && playerChoice == 1 || computerChoice == 1 && playerChoice == 2 || computerChoice == 2 && playerChoice == 0 )
-    {
-      console.log(`Computer choice (${computerChoice}): ${choices[computerChoice]}\nPlayer Choice (${playerChoice}): ${choices[playerChoice]}\nPlayer Wins`);
-      return 2;
-    }
-
-  // computer wins, return 1
-  else if (computerChoice == 0 && playerChoice == 2 || computerChoice == 1 && playerChoice == 0 || computerChoice == 2 && playerChoice == 1 )
-    {
-      console.log(`Computer choice (${computerChoice}): ${choices[computerChoice]}\nPlayer Choice (${playerChoice}): ${choices[playerChoice]}\nComputer Wins`);
-      return 1;
-    }
 }
 
 function updateScore()
@@ -83,32 +37,4 @@ function updateScore()
     {
       playerScore++;
     }
-}
-
-function playGame(numberOfRounds)
-{
-  /* This function loops through the updateScore() function depending on the value of the
-     numberOfRounds parameter. for example numberOfRounds = 5 would create 5 rounds
-  */
-  for (let round = 0; round < numberOfRounds; round++)
-    {
-      console.log(`Round ${round+1}\n`) // Displays current round
-      updateScore();
-    }
-
-    console.log(`Computer Score: ${computerScore}\nPlayer Score: ${playerScore}`)
-
-    if (computerScore > playerScore)
-      {
-        console.log("Computer Wins!");
-      }
-    else if (computerScore < playerScore)
-      {
-        console.log("Player Wins!");
-      }
-    else if (computerScore == playerScore)
-      {
-        console.log("Draw!")
-      }
-
 }
