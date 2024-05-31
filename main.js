2356bef15d1e70d6cd073cb1209dce9fc5846223
shakey200592 <shakey1992jm@hotmail.co.uk>
2024-05-31 22:18:56 +0100

    feat: Complete player and computer selection logic

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-// Variables
-const choices = ["rock", "paper", "scissors"]; // Store choices in a string array
+/////// Variables ///////
+const choices = ["Rock", "Paper", "Scissors"]; // Store choices in a string array
 let computerScore = 0;
 let playerScore = 0;
+let playerChoiceInt;
+let playerChoiceString;
+let randomeChoiceInt;
+let randomeChoiceString;
+let draws = 0;
 
+/////// Functions ///////
 
 // Create Rock Paper Scissor UI using JS only
 function createElement(parentElement, childElement, className ="", idName ="", textContent = "") {
@@ -33,45 +39,34 @@ function createElement(parentElement, childElement, className ="", idName ="", t
 
 }
 
-// Create required Elements, classes/ids and text content
-createElement("body", "div", "button-container"); // <div class = "game-container">
-createElement("div", "button", "rock", "", "Rock"); // <button id="rock">
-createElement("div", "button", "paper", "", "Paper"); // <button id="paper">
-createElement("div", "button", "scissors", "", "Scissors"); // <button id="scissors">
-
-createElement("body", "div", "score-container", "player-text", "Player Wins: ");
-createElement("body", "div", "score-container", "computer-text", "Computer Wins: ");
-createElement("body", "div", "score-container", "draw-text", "Draws: ");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
+function getplayerChoice () {
+  const buttons = document.querySelectorAll(".choice");
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      playerChoiceString = document.querySelector("#player-choice")
+      playerChoiceString.textContent = `Player Choice: ${event.target.textContent}`;
+      playerChoiceInt = choices.indexOf(event.target.textContent);
+      console.log(`Player choice (${playerChoiceInt}): ${playerChoiceString.textContent}`);
+      
+    })
+  })
+}
 
 // Functions for game functionality
 function getComputerChoice(arr_of_choices) {
   // This function returns a random string from the values stored in the "choices" array
 
-  // Select random number between 0 and 2
-  let randomeChoice = Math.floor(Math.random() * arr_of_choices.length); 
-
-  // Returns string based on the "arr_of_choices" parameter
-  // and returns a string with the first char capitalised
-  return randomeChoice;
+  const buttons = document.querySelectorAll(".choice");
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      // Select random number between 0 and 2
+      randomeChoiceInt = (Math.floor(Math.random() * arr_of_choices.length));
+      randomeChoiceString = arr_of_choices[randomeChoiceInt];
+      let computerChoiceText = document.querySelector("#computer-choice");
+      computerChoiceText.textContent = `Computer Choice: ${randomeChoiceString}`;
+      console.log(`Computer choice (${randomeChoiceInt}): ${computerChoiceText.textContent}`);
+    })
+  })
 }
 
 function playRound(computerChoice, playerChoice) {
@@ -96,4 +91,28 @@ function updateScore()
     }
 }
 
-*/
+
+
+
+// Create required Elements, classes/ids and text content
+createElement("body", "div", "button-container"); // <div class = "game-container">
+createElement("div", "button", "choice", "rock", "Rock"); // <button id="rock">
+createElement("div", "button", "choice", "paper", "Paper"); // <button id="paper">
+createElement("div", "button", "choice", "scissors", "Scissors"); // <button id="scissors">
+createElement("div", "button", "choice", "reset", "Reset Score"); 
+
+createElement("body", "div", "score-container", "", "Scoreboard: "); // <div class = "score-container"></div>
+createElement(".score-container", "p", "", "player-wins", `Player Wins: ${playerScore}`); // <p>Player Wins: {}</p>
+createElement(".score-container", "p", "", "computer-wins", `Computer Wins: ${computerScore}`);// <p>Computer Wins: {}</p>
+createElement(".score-container", "p", "", "draws", `Computer Wins: ${draws}`); //<p>Draws: <> </p>
+
+createElement("body","p","","player-choice",`Player Choice:`);
+createElement("body","p","","computer-choice",`Computer Choice:`);
+
+// Play Game
+getplayerChoice();
+getComputerChoice(choices);
+
+
+
+
